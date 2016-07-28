a24b7d89be42709509dad868b32305d2d3b7249d
Jesus Flores <joy_warmgun@hotmail.com>
2016-07-28 18:45:02 -0500

    Arreglando archivo thermal_zone.c para HB 	modified:   thermal_zone.c

--- FanCtlModule/thermal_zone.c
+++ FanCtlModule/thermal_zone.c
@@ -5,22 +5,11 @@
 // Estructura del thermal zone que se usara en el modulo
 struct thermal_zone_device *fcm_tz;
 
-// Lista enlazada que exporte en el archivo drivers/thermal/thermal_core.c
-extern struct list_head thermal_tz_list;
-
 int obtiene_thermal_zone()
 {
-	//struct thermal_zone_device *pos = NULL;
-	/*
-	// recorriendo la lista para sacar el primer elemento
-	list_for_each_entry(pos, &thermal_tz_list, node)
-	{
-		fcm_tz = pos;
-		break;
-	}
-	*/
+	fcm_tz = thermal_zone_get_zone_by_name("imx_thermal_zone");
+	
 	// Si no se encontro el thermal zone
-	fcm_tz=thermal_zone_get_zone_by_name ("imx_thermal_zone");
 	if (IS_ERR(fcm_tz))
 	{		
 		printk(KERN_INFO "FanCtlModule: No se encontro una thermal_zone\n");
